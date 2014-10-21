3bffc8e76447ab367bcab82c856eeafad7ae4ef5
Christoph Kleineweber <kleineweber@zib.de>
2014-10-21 14:48:33 +0200

    Travis-CI integration

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+docker build -t xtreemfs/xtreemfs-common xtreemfs-common
+docker build -t xtreemfs/xtreemfs-dir xtreemfs-dir
+docker build -t xtreemfs/xtreemfs-mrc xtreemfs-mrc
+docker build -t xtreemfs/xtreemfs-osd xtreemfs-osd
